5a7229fbc70fd3077b2747d00e70d923af395047
Alena <sharko_alena@mail.ru>
2016-02-08 12:49:57 +0300

    more tests

--- src/test/java/com.scrumtrek.simplestore/CustomerTest.java
+++ src/test/java/com.scrumtrek.simplestore/CustomerTest.java
@@ -1,17 +1,87 @@
 package com.scrumtrek.simplestore;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CustomerTest {
 
-    @Test
-    public void statementTest() {
-        Customer testCustomer = new Customer("test");
-        Movie movie1 = new Movie("testMovie", PriceCodes.NewRelease);
-        Rental rental = new Rental(movie1, 1);
+    private Customer testCustomer;
+    private Movie movie;
 
-        testCustomer.addRental(rental);
+    @Before
+    public void setUp() {
+        testCustomer = new Customer("test");
+        movie = new Movie("testMovie", PriceCodes.Regular);
+    }
+
+    @Test
+    public void statementNewReleaseTest() {
+        movie.setPriceCode(PriceCodes.NewRelease);
+        testCustomer.addRental(new Rental(movie, 1));
+        String actualString = testCustomer.Statement();
+
+        Assert.assertEquals("Rental record for test\n" +
+                "\ttestMovie\t3.0\n" +
+                "Amount owed is 3.0\n" +
+                "You earned 1 frequent renter points.", actualString);
+    }
+
+    @Test
+    public void statementNewReleaseWithMoreDaysTest() {
+        movie.setPriceCode(PriceCodes.NewRelease);
+        testCustomer.addRental(new Rental(movie, 5));
+        String actualString = testCustomer.Statement();
+
+        Assert.assertEquals("Rental record for test\n" +
+                "\ttestMovie\t15.0\n" +
+                "Amount owed is 15.0\n" +
+                "You earned 2 frequent renter points.", actualString);
+    }
+
+    @Test
+    public void statementRegularMinTest() {
+        movie.setPriceCode(PriceCodes.Regular);
+        testCustomer.addRental(new Rental(movie, 1));
+
+        String actualString = testCustomer.Statement();
+
+        Assert.assertEquals("Rental record for test\n" +
+                "\ttestMovie\t2.0\n" +
+                "Amount owed is 2.0\n" +
+                "You earned 1 frequent renter points.", actualString);
+    }
+
+    @Test
+    public void stateRegularMaxTest() {
+        movie.setPriceCode(PriceCodes.Regular);
+        testCustomer.addRental(new Rental(movie, 5));
+
+        String actualString = testCustomer.Statement();
+
+        Assert.assertEquals("Rental record for test\n" +
+                "\ttestMovie\t6.5\n" +
+                "Amount owed is 6.5\n" +
+                "You earned 1 frequent renter points.", actualString);
+    }
+
+    @Test
+    public void stateChildrensMinTest() {
+        movie.setPriceCode(PriceCodes.Childrens);
+        testCustomer.addRental(new Rental(movie, 2));
+
+        String actualString = testCustomer.Statement();
+
+        Assert.assertEquals("Rental record for test\n" +
+                "\ttestMovie\t1.5\n" +
+                "Amount owed is 1.5\n" +
+                "You earned 1 frequent renter points.", actualString);
+    }
+
+    @Test
+    public void stateChildrensMaxTest() {
+        movie.setPriceCode(PriceCodes.Childrens);
+        testCustomer.addRental(new Rental(movie, 5));
 
         String actualString = testCustomer.Statement();
 
